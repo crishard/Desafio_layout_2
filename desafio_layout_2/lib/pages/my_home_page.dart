35a3adb112b7e45404625a5d2d7929997f61bdc2
crishard <crislantorres9@gmail.com>
2022-07-28 00:08:11 -0300

    adicionando navigationbar na home page

--- desafio_layout_2/lib/pages/my_home_page.dart
+++ desafio_layout_2/lib/pages/my_home_page.dart
@@ -1,4 +1,6 @@
+import 'package:desafio_layout_2/widgets/home_page_navigationbar.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -7,6 +9,7 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 class _MyHomePageState extends State<MyHomePage>{
+   int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +18,16 @@ class _MyHomePageState extends State<MyHomePage>{
         children: [
           
         ],
+      ),
+      bottomNavigationBar: HomePageButtonNavigationBar(
+        onTap: (index) => setState(() => _currentIndex = index),
+        currentIndex: _currentIndex,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(FontAwesomeIcons.qrcode),
       ),
     );
   }
