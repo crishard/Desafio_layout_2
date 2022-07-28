776781e253be362a6713f6766612f17308cdcd82
crishard <crislantorres9@gmail.com>
2022-07-28 00:03:35 -0300

    criando e estruturando arquivo do bottomnavigatiobar

---
+++ desafio_layout_2/lib/widgets/home_page_navigationbar.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomePageButtonNavigationBar extends StatelessWidget {
+ 
+  const HomePageButtonNavigationBar({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(items: [],
+    );
+  }
+}
