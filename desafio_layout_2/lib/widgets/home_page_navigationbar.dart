b8f98064246f642f66f8a543bb20a21a32de8f0f
crishard <crislantorres9@gmail.com>
2022-07-28 00:06:43 -0300

    navigationbar completo

--- desafio_layout_2/lib/widgets/home_page_navigationbar.dart
+++ desafio_layout_2/lib/widgets/home_page_navigationbar.dart
@@ -2,11 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomePageButtonNavigationBar extends StatelessWidget {
- 
-  const HomePageButtonNavigationBar({Key? key}) : super(key: key);
+  const HomePageButtonNavigationBar({Key? key, required this.currentIndex, required this.onTap}) : super(key: key);
+
+  final Function(int) onTap;
+  final int currentIndex;
+
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(items: [],
+    return BottomNavigationBar(
+      onTap: onTap,
+      currentIndex: currentIndex,
+      type: BottomNavigationBarType.fixed,
+      items: const [
+        BottomNavigationBarItem(
+          label: "Explore",
+          icon: Icon(Icons.explore),
+        ),
+        BottomNavigationBarItem(
+          label: "Map",
+          icon: Icon(Icons.location_on),
+        ),
+        BottomNavigationBarItem(
+          label: "Ticket",
+          icon: Icon(FontAwesomeIcons.ticketAlt),
+        ),
+        BottomNavigationBarItem(
+          label: "User",
+          icon: Icon(Icons.person),
+        ),
+      ],
     );
   }
 }
