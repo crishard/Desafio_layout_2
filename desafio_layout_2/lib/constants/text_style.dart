99c64c56a5ab0272966e849f2f4ff44208fee180
crishard <crislantorres9@gmail.com>
2022-08-03 10:03:53 -0300

    criando arquivo de estilo de texto e realizando mudanças

--- desafio_layout_2/lib/constants/text_style.dart
+++ desafio_layout_2/lib/constants/text_style.dart
@@ -2,6 +2,6 @@ import 'package:desafio_layout_2/constants/colors.dart';
 import 'package:flutter/material.dart';
 
 const headerStyle = TextStyle(color: Colors.black, fontSize: 24, fontWeight: FontWeight.bold);
-const titleStyle = TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold);
-const subtitleStyle = TextStyle(color: Colors.grey, fontSize: 14);
-const monthStyle = TextStyle(color: orange, fontSize: 16, fontWeight: FontWeight.w500);
+const titleStyle = TextStyle(color: Colors.black, fontSize: 16, fontWeight: FontWeight.bold);
+const subtitleStyle = TextStyle(color: Colors.grey, fontSize: 13);
+const monthStyle = TextStyle(color: orange, fontSize: 14, fontWeight: FontWeight.w500);
