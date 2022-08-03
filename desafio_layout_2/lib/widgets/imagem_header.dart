6c099f12cb577cf11ae9da333935ccb0a2b317c8
crishard <crislantorres9@gmail.com>
2022-08-03 10:09:11 -0300

    criando barra superior usando imagem

---
+++ desafio_layout_2/lib/widgets/imagem_header.dart
@@ -0,0 +1,44 @@
+import 'package:desafio_layout_2/widgets/barra_interacao.dart';
+import 'package:desafio_layout_2/widgets/images_evento.dart';
+import 'package:flutter/material.dart';
+
+class ImagemHeader extends StatelessWidget {
+  ImagemHeader({Key? key, required this.imageSize, required this.image, required this.controller, required this.scale, required this.favorito, required this.nome, required this.funcao,}) : super(key: key);
+  final AnimationController controller;
+  final Animation<double> scale;
+  final dynamic nome;
+  final Function()? funcao;
+  final bool favorito;
+  bool hasTitle = false;
+    double imageSize;
+  final dynamic image;
+
+  
+  double minimumScale = 0.8;
+  @override
+  Widget build(BuildContext context) {
+    double maxHeight = MediaQuery.of(context).size.height;
+    return GestureDetector(
+      onVerticalDragUpdate: (detail) {
+        controller.value += detail.primaryDelta! / maxHeight * 2;
+      },
+      onVerticalDragEnd: (detail) {
+        if (scale.value > minimumScale) {
+          controller.reverse();
+        } else {
+          Navigator.of(context).pop();
+        }
+      },
+      child: Stack(
+        children: <Widget>[
+          ImagemEvento(imageSize: imageSize, image: image),
+          BarraInteracao(nome: nome, funcao: funcao, controller: controller, favorito: favorito, hasTitle: hasTitle,),
+        ],
+      ),
+    );
+    
+  }
+}
+
+
+
---
+++ desafio_layout_2/lib/widgets/images_evento.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ImagemEvento extends StatelessWidget {
+  ImagemEvento({Key? key, required this.imageSize, required this.image}) : super(key: key);
+  double imageSize;
+  final dynamic image;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      height: imageSize,
+      child: Hero(
+        tag: image,
+        child: ClipRRect(
+          borderRadius: const BorderRadius.vertical(bottom: Radius.circular(32)),
+          child: Image.network(
+            image,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
