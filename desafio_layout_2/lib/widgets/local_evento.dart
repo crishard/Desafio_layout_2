c6ccd490d02b63ea3424dacba903d85946c54d1b
crishard <crislantorres9@gmail.com>
2022-08-03 10:05:43 -0300

    criando card de local do evento

---
+++ desafio_layout_2/lib/widgets/local_evento.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class LocalEvento extends StatelessWidget {
+  const LocalEvento({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width * 0.8;
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(12),
+      child: Image.asset(
+        'assets/images/map.jpg',
+        height: MediaQuery.of(context).size.height / 2,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
+
+
+
+
+
