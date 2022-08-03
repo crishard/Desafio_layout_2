80ab06ee584d1667b58ea447b09f4800165c1d83
crishard <crislantorres9@gmail.com>
2022-08-03 10:07:03 -0300

    criando card de data do evento

---
+++ desafio_layout_2/lib/widgets/data_evento.dart
@@ -0,0 +1,40 @@
+import 'package:desafio_layout_2/constants/colors.dart';
+import 'package:desafio_layout_2/constants/text_style.dart';
+import 'package:desafio_layout_2/models/evento_model.dart';
+import 'package:desafio_layout_2/utils/date_time_utils.dart';
+import 'package:desafio_layout_2/widgets/adicionar_ao_calendario.dart';
+import 'package:desafio_layout_2/widgets/horario_evento.dart';
+import 'package:desafio_layout_2/widgets/sizebox_help.dart';
+import 'package:flutter/material.dart';
+
+class DataEvento extends StatelessWidget {
+  const DataEvento({Key? key, required this.diaDaSemana, required this.diaDoMes, required this.mes}) : super(key: key);
+  final dynamic mes;
+  final dynamic diaDoMes;
+  final dynamic diaDaSemana;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment:  MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+            color: primaryLight,
+            borderRadius: BorderRadius.circular(8),
+          ),
+          padding: const EdgeInsets.all(6),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(mes, style: monthStyle),
+              Text(diaDoMes, style: titleStyle),
+            ],
+          ),
+        ),
+        SizeHelper.horizontalSpace(12),
+        HorarioEvento(diaDaSemana:diaDaSemana ),
+        const AdicionarAoCalendario(),
+            ],
+    );
+  }
+}
