c6baeb487a14a69d82da0813aad3aed350800d7c
crishard <crislantorres9@gmail.com>
2022-08-03 10:08:41 -0300

    criando card de informações de organiação e informações de preço do  evento

---
+++ desafio_layout_2/lib/widgets/informacoes_organizacao.dart
@@ -0,0 +1,43 @@
+import 'package:desafio_layout_2/constants/colors.dart';
+import 'package:desafio_layout_2/constants/text_style.dart';
+import 'package:desafio_layout_2/widgets/sizebox_help.dart';
+import 'package:flutter/material.dart';
+
+class InformacoesOrganizacao extends StatelessWidget {
+  const InformacoesOrganizacao({Key? key, required this.organizacao1, required this.organizadorEvento}) : super(key: key);
+  final dynamic organizacao1;
+  final dynamic organizadorEvento;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        CircleAvatar(
+          child: Text(organizacao1),
+        ),
+        SizeHelper.horizontalSpace(16),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(organizadorEvento, style: titleStyle),
+            SizeHelper.verticalSpace(4),
+            const Text("Organizer", style: subtitleStyle),
+          ],
+        ),
+        const Spacer(),
+        TextButton(
+          child: Text("Follow", style: TextStyle(color: Theme.of(context).primaryColor)),
+          onPressed: () {},
+          style: TextButton.styleFrom(
+            shape: const StadiumBorder(),
+            primary: primaryLight,
+          ),
+        )
+      ],
+    );
+  }
+}
+
+
+
+
+
---
+++ desafio_layout_2/lib/widgets/informacoes_preco.dart
@@ -0,0 +1,42 @@
+import 'package:desafio_layout_2/constants/text_style.dart';
+import 'package:desafio_layout_2/widgets/compra_ingresso.dart';
+import 'package:desafio_layout_2/widgets/sizebox_help.dart';
+import 'package:flutter/material.dart';
+
+class InformacoesPreco extends StatelessWidget {
+
+  const InformacoesPreco({Key? key, required this.preco}) : super(key: key);
+  final dynamic preco;
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width * 0.8;
+    return Container(
+      padding: const EdgeInsets.all(16),
+      color: Colors.white,
+      width: MediaQuery.of(context).size.width,
+      child: Row(
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              const Text("Price", style: subtitleStyle),
+              SizeHelper.verticalSpace(8),
+              RichText(
+                text: TextSpan(
+                  children: [
+                    TextSpan(
+                        text: "\$${preco}", style: titleStyle.copyWith(color: Theme.of(context).primaryColor)),
+                    const TextSpan(text: "/per person", style: TextStyle(color: Colors.black)),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const Spacer(),
+          const CompraIngresso(),
+        ],
+      ),
+    );
+  }
+}
