7b02c9af5b50c0975d5c2d3db88440f787e75be2
crishard <crislantorres9@gmail.com>
2022-08-03 10:06:32 -0300

    criando card adicionadno ao calendário

---
+++ desafio_layout_2/lib/widgets/adicionar_ao_calendario.dart
@@ -0,0 +1,26 @@
+import 'package:desafio_layout_2/constants/colors.dart';
+import 'package:desafio_layout_2/constants/text_style.dart';
+import 'package:desafio_layout_2/widgets/sizebox_help.dart';
+import 'package:flutter/material.dart';
+
+class AdicionarAoCalendario extends StatelessWidget {
+  const AdicionarAoCalendario({Key? key,}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 6),
+          decoration: const ShapeDecoration(shape: StadiumBorder(), color: primaryLight),
+          child: Row(
+            children:[
+              SizeHelper.horizontalSpace(12),
+              Text("Add to", style: subtitleStyle.copyWith(color: Theme.of(context).primaryColor)),
+              FloatingActionButton(
+                mini: true,
+                onPressed: () {},
+                child: const Icon(Icons.calendar_month_outlined),
+              ),
+            ]
+          )
+    );
+  }
+}
